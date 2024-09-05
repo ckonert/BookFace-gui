63afa78ed14c7b04d81e94355f184a6e8d02a0d2
Cris Konert <20204082+ckonert@users.noreply.github.com>
2024-09-05 13:52:43 +0200

    updating tests and added tailwind framework

--- src/app/components/author-form/author-form.component.spec.ts
+++ src/app/components/author-form/author-form.component.spec.ts
@@ -1,23 +1,68 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthorFormComponent } from './author-form.component';
+import { AuthorService } from '../../services/author.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 describe('AuthorFormComponent', () => {
   let component: AuthorFormComponent;
   let fixture: ComponentFixture<AuthorFormComponent>;
+  let service: AuthorService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AuthorFormComponent]
+      declarations: [ AuthorFormComponent ],
+      imports: [ReactiveFormsModule, HttpClientTestingModule],
+      providers: [AuthorService]
     })
     .compileComponents();
-    
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(AuthorFormComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(AuthorService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have an invalid form when required fields are empty', () => {
+    expect(component.authorForm.valid).toBeFalsy();
+
+    const lastname = component.authorForm.controls['lastname'];
+    const firstname = component.authorForm.controls['firstname'];
+
+    lastname.setValue('');
+    firstname.setValue('');
+
+    expect(component.authorForm.valid).toBeFalsy();
+  });
+
+  it('should have a valid form when required fields are filled', () => {
+    const lastname = component.authorForm.controls['lastname'];
+    const firstname = component.authorForm.controls['firstname'];
+
+    lastname.setValue('Martin');
+    firstname.setValue('George RR');
+
+    expect(component.authorForm.valid).toBeTruthy();
+  });
+
+  // it('should call the service to save the author when form is valid', () => {
+  //   spyOn(service, 'createAuthor').and.returnValue(of({}));
+
+  //   const lastname = component.authorForm.controls['lastname'];
+  //   const firstname = component.authorForm.controls['firstname'];
+
+  //   lastname.setValue('Martin');
+  //   firstname.setValue('George RR');
+
+  //   component.onSubmit();
+
+  //   expect(service.createAuthor).toHaveBeenCalled();
+  // });
 });
--- src/app/components/author-list/author-list.component.spec.ts
+++ src/app/components/author-list/author-list.component.spec.ts
@@ -1,23 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AuthorListComponent } from './author-list.component';
+import { AuthorService } from '../../services/author.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
 describe('AuthorListComponent', () => {
   let component: AuthorListComponent;
   let fixture: ComponentFixture<AuthorListComponent>;
+  let service: AuthorService;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AuthorListComponent]
+      declarations: [ AuthorListComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [AuthorService]
     })
     .compileComponents();
-    
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(AuthorListComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(AuthorService);
+    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch authors from the service on init', () => {
+    const mockAuthors = [
+      { id: 1, lastname: 'Tolkien', firstname: 'JRR', imageUrl: '' },
+      { id: 2, lastname: 'Martin', firstname: 'George RR', imageUrl: '' }
+    ];
+
+    spyOn(service, 'getAuthors').and.returnValue(of(mockAuthors));
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(component.authors.length).toBe(2);
+    expect(component.authors).toEqual(mockAuthors);
   });
 });

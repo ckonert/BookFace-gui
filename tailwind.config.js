4fe7454c09f3d3933c92f6e5a5eab7a41af593b8
Cris Konert <20204082+ckonert@users.noreply.github.com>
2024-09-09 15:33:18 +0200

    cleaned tailwind up a bit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,4 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      // Set font family
-      fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      },
-      // Set theme colors (Required config!)
-      colors: {
-        primary: colors.blue,
-        secondary: colors.slate,
-      },
-    },
-  },
-  fontFamily: {
-    sans: ["Inter", ...defaultTheme.fontFamily.sans],
-  },
-  // Add plugins
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms")]
 };

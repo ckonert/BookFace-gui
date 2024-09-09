5d06d29b41c5075997020119122fe0db980465c0
Cris Konert <20204082+ckonert@users.noreply.github.com>
2024-09-09 16:34:48 +0200

    alias for intercept in test

--- cypress/e2e/showBookList.cy.ts
+++ cypress/e2e/showBookList.cy.ts
@@ -2,12 +2,15 @@ describe('Display the list of books', () => {
   describe('Show the booklist', () => {
 
     beforeEach(() => {
-      cy.intercept('GET', '**/', {fixture: 'booklist.json'});
+      cy.intercept('GET', '**/', { fixture: 'booklist.json' }).as('getBooks');
       cy.visit('http://localhost:4200/books');
     });
 
     it('should show a list of four books', () => {
+      cy.wait('@getBooks');
       cy.get('#booklist').should('exist');
+      cy.get('#book').should('be.visible');
+      cy.get('#booklist').find('#book').should('have.length', 4);
     });
   });
 });

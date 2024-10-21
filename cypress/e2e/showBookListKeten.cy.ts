528180dff8066a52e4a975db03e1f706da2289e0
Cris Konert <20204082+ckonert@users.noreply.github.com>
2024-10-21 10:48:18 +0200

    author list and form updated

--- cypress/e2e/showBookListKeten.cy.ts
+++ cypress/e2e/showBookListKeten.cy.ts
@@ -2,13 +2,11 @@ describe('Display the list of books', () => {
   describe('Show the booklist', () => {
 
     beforeEach(() => {
-      cy.intercept('GET', '**/', { fixture: 'booklist.json' }).as('getBooks');
       cy.visit('http://localhost:4200/books');
     });
 
     it('should show a list of four books', () => {
-      cy.wait('@getBooks');
-      cy.get('#booklist #book').should('have.length', 4);
+      cy.get('#booklist #book').should('have.length', 5);
     });
   });
 });
--- src/app/components/author-form/author-form.component.ts
+++ src/app/components/author-form/author-form.component.ts
@@ -14,6 +14,9 @@ import { CommonModule } from '@angular/common';
 export class AuthorFormComponent implements OnInit {
   authorForm: FormGroup;
   authorId?: number;
+  message: string = '';      // Message text
+  messageType: string = '';  // 'success' or 'error'
+  imageUrlPreview: string = '';  // Preview URL for the image
 
   constructor(
     private authorService: AuthorService,
@@ -32,17 +35,44 @@ export class AuthorFormComponent implements OnInit {
     if (this.authorId) {
       this.authorService.getAuthor(this.authorId).subscribe(data => {
         this.authorForm.patchValue(data);
+        this.imageUrlPreview = data.imageUrl;  // Set initial image preview if editing
       });
     }
+
+    // Listen to changes in the imageUrl field and update the preview
+    this.authorForm.get('imageUrl')?.valueChanges.subscribe((url: string) => {
+      this.imageUrlPreview = url;  // Update the preview URL as user types
+    });
+   
   }
 
   saveAuthor(): void {
+
     if (this.authorForm.valid) {
       if (this.authorId) {
-        this.authorService.updateAuthor(this.authorId, this.authorForm.value).subscribe();
+        this.authorService.updateAuthor(this.authorId, this.authorForm.value).subscribe(
+          () => {
+            this.message = 'The author has been updated successfully!';
+            this.messageType = 'success';
+          },
+          (error) => {
+            this.message = 'Something went wrong while updating the author.';
+            this.messageType = 'error';
+          }
+        );
       } else {
-        this.authorService.createAuthor(this.authorForm.value).subscribe();
+        this.authorService.createAuthor(this.authorForm.value).subscribe(
+          () => {
+            this.message = 'The author has been created successfully!';
+            this.messageType = 'success';
+          },
+          (error) => {
+            this.message = 'Something went wrong while creating the author.';
+            this.messageType = 'error';
+          }
+        );
       }
     }
+
   }
 }
